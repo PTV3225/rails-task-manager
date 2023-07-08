77f7b2c6192850aabddb756c2ebfdf5074b08fba
Peter Varley <petertvarley@gmail.com>
2023-07-08 10:31:16 +1000

    created taskmaster app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get "tasks", to: "pages#task"
+  # get "tasks/new", to: "pages#new"
+  # post "tasks", to: "pages#create"
+  # get "tasks/:id", to: "pages#show", as: :task
+  # get "tasks/:id/edit", to: "pages#edit", as: :edit_task
+  # patch "tasks/:id", to: "pages#update"
+  # delete "tasks/:id", to: "pages#destroy", as: :delete_task
 end
